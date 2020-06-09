2131b41d4f1e9277a254c7e27441f5d28ab06217
Hubert Salamaga <hubert.salamaga@htdevelopers.com>
2020-06-09 17:34:46 +0200

    Prepare setUp for tests; Add first test

--- testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
@@ -1,15 +1,41 @@
 package edu.iis.mto.testreactor.dishwasher;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import edu.iis.mto.testreactor.dishwasher.engine.Engine;
+import edu.iis.mto.testreactor.dishwasher.pump.WaterPump;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class DishWasherTest {
 
+    @Mock
+    WaterPump waterPump;
+    @Mock
+    Engine engine;
+    @Mock
+    DirtFilter dirtFilter;
+    @Mock
+    Door door;
+
+    DishWasher dishWasher;
+
+    @BeforeEach
+    void setUp() {
+        dishWasher = new DishWasher(waterPump, engine, dirtFilter, door);
+    }
+
     @Test
-    public void itCompiles() {
-        assertThat(true, Matchers.equalTo(true));
+    public void start_programIsNull_ThrowsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> {
+            dishWasher.start(null);
+        });
     }
 
 }
