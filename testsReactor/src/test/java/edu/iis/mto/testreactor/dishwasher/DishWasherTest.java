a9c5fe70ac76968b2d620c76058efbf5aa48004a
Hubert Salamaga <hubert.salamaga@htdevelopers.com>
2020-06-09 17:57:12 +0200

    Add next state tests

--- testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
@@ -1,9 +1,8 @@
 package edu.iis.mto.testreactor.dishwasher;
 
-import static edu.iis.mto.testreactor.dishwasher.Status.DOOR_OPEN;
+import static edu.iis.mto.testreactor.dishwasher.Status.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.samePropertyValuesAs;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.lenient;
 
@@ -26,14 +25,15 @@ public class DishWasherTest {
     DirtFilter dirtFilter;
     @Mock
     Door door;
-    @Mock
-    ProgramConfiguration programConfiguration;
 
+    ProgramConfiguration programConfiguration;
     DishWasher dishWasher;
 
     @BeforeEach
     void setUp() {
         dishWasher = new DishWasher(waterPump, engine, dirtFilter, door);
+        WashingProgram unrelevantWashingProgram = WashingProgram.ECO;
+        programConfiguration = programConfiguration(unrelevantWashingProgram, true);
     }
 
     @Test
@@ -44,16 +44,49 @@ public class DishWasherTest {
     }
 
     @Test
-    public void start_doorIsNotClosed_ReturnDoorOpenError() {
+    public void start_doorIsNotClosed_ReturnsDoorOpenError() {
         lenient().when(door.closed()).thenReturn(false);
         RunResult result = dishWasher.start(programConfiguration);
 
         assertThat(error(DOOR_OPEN), samePropertyValuesAs(result));
     }
 
+    @Test
+    public void start_dirtFilterisFilled_ReturnsFilterError() {
+        lenient().when(door.closed()).thenReturn(true);
+        lenient().when(dirtFilter.capacity()).thenReturn(30.0d);
+        RunResult result = dishWasher.start(programConfiguration);
+
+        assertThat(error(ERROR_FILTER), samePropertyValuesAs(result));
+    }
+
+    @Test
+    public void start_doorAreClosedAndDirtFilterIsNotFilled_ReturnsSuccess() {
+        lenient().when(door.closed()).thenReturn(true);
+        lenient().when(dirtFilter.capacity()).thenReturn(70.0d);
+        RunResult result = dishWasher.start(programConfiguration);
+
+        assertThat(success(programConfiguration.getProgram()), samePropertyValuesAs(result));
+    }
+
     private RunResult error(Status errorPump) {
         return RunResult.builder()
                         .withStatus(errorPump)
                         .build();
+    }
+
+    private RunResult success(WashingProgram program) {
+        return RunResult.builder()
+                        .withStatus(SUCCESS)
+                        .withRunMinutes(program.getTimeInMinutes())
+                        .build();
+    }
+
+    private ProgramConfiguration programConfiguration(WashingProgram program, boolean tabletsUsed) {
+        return ProgramConfiguration.builder()
+                                   .withProgram(program)
+                                   .withTabletsUsed(tabletsUsed)
+                                   .withFillLevel(FillLevel.HALF)
+                                   .build();
     }
 }
